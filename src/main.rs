0be93b3d1f51d7a7876f04012f9442a78e884ab4
walidmeguenni <walidmg125@gmail.com>
2024-01-26 09:20:24 +0100

    refactor: remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-// use std::thread;
-// use std::time::Duration;
 mod connect;
 
 fn main() {
     loop {
-        // println!("Hello, world!");
-        // thread::sleep(Duration::from_secs(1));
         connect::sql_connect_options::connect();
     }
 }
